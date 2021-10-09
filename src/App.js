db4ec6395e2ff11f6cc6a24d513f4217bb5a5db3
praneeth_alluri <praneeth.alluri12@gmail.com>
2021-10-09 19:14:50 +0530

    design fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState , useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import db from './Firebase';
 import Message from './Message'
@@ -6,58 +6,57 @@ import firebase from 'firebase';
 import FlipMove from 'react-flip-move';
 import SendIcon from '@material-ui/icons/Send';
 import { IconButton } from '@material-ui/core';
-import {Input} from '@material-ui/core'
-import {FormControl} from '@material-ui/core'
+import { Input } from '@material-ui/core';
+import { FormControl } from '@material-ui/core';
 
 function App() {
   const [input, setInput] = useState('');
-const [messages, setMessages] = useState([]);
-const [username, setUsername] = useState();
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState();
 
-useEffect(() => {
-  db.collection('messages')
-  .orderBy('timestamp', 'desc')
-  .onSnapshot(snapShot =>{
-    setMessages(snapShot.docs.map(doc => ({id : doc.id, message : doc.data()})))
-  });
-}, [])
+  useEffect(() => {
+    db.collection('messages')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot(snapShot => {
+        setMessages(snapShot.docs.map(doc => ({ id: doc.id, message: doc.data() })))
+      });
+  }, [])
 
-useEffect(() => {
-  setUsername(prompt('please enter your name'));
+  useEffect(() => {
+    setUsername(prompt('please enter your name'));
   }, [])
 
 
-  const sendMesssage=(e)=>{
+  const sendMesssage = (e) => {
     e.preventDefault();
     db.collection('messages').add({
-      message:input,
-      username : username,
-      timestamp : firebase.firestore.FieldValue.serverTimestamp()
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
     })
     setInput('');
   }
   return (
     <div className="app">
-      <h2>Hello World</h2>
-      <h2>Welcome {username}</h2>
+      <h2 style={{ color: "green" }}>Welcome <span style={{ color: "orange", textTransform: "uppercase" }}> {username} </span> </h2>
       <form className="app__form">
-      <FormControl className="app__formControl">
+        <FormControl className="app__formControl">
 
-      <Input placeholder="enter a message.." className="app__input" value={input} onChange= {(e) =>setInput(e.target.value) } />
-        <IconButton disabled={!input} type="submit" className="app__iconButton" onClick={sendMesssage}>
-        <SendIcon />
-        </IconButton>
-      </FormControl>
-        
-    </form>
-    <FlipMove>
+          <Input placeholder="Enter a message.." className="app__input" value={input} onChange={(e) => setInput(e.target.value)} />
+          <IconButton disabled={!input} type="submit" className="app__iconButton" onClick={sendMesssage}>
+            <SendIcon />
+          </IconButton>
+        </FormControl>
 
-      {
-        messages.map(({id, message}) => (
-          <Message key={id} username={username}  message={message} />
+      </form>
+      <FlipMove>
+
+        {
+          messages.map(({ id, message }) => (
+            <Message key={id} username={username} message={message} />
           ))
         }
-        </FlipMove>
+      </FlipMove>
     </div>
   );
 }
